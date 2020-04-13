81ab8cc8adfaeb076faee44e11d5e8212b454586
Christian Shaw <cshaw00048@gmail.com>
2020-04-13 05:10:20 -0500

    requiring keys.js to access spotify api

--- liri.js
+++ liri.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
+var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
-var spotify = new Spotify({
-    id: "9e1a1d4373af4b618b59e85f26118f87",
-    secret: "d5e37f95db0045d8b0d8853c43bcf166"
-  });
+var spotify = new Spotify(keys.spotify);
+
   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
