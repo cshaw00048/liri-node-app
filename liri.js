2a6b553be603e07206523e2be6e779194d1a387c
Christian Shaw <cshaw00048@gmail.com>
2020-04-13 05:31:48 -0500

    updating .env file and deleting unneeded code from liri.js

--- liri.js
+++ liri.js
@@ -1,22 +1,10 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
 
-  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data); 
-  });
 
-  var omdb = "http://www.omdbapi.com/?apikey=a7aa0484"
-  axios.get(omdb + "&t=batman") 
-  .then(function(res) {
-      console.log(res.data);
-  })
+
 
 
 
