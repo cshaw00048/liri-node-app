70860903178b26bf0be0614c3b3706aafd0b8c6d
Christian Shaw <cshaw00048@gmail.com>
2020-04-13 16:38:02 -0500

    update 6 to liri.js

--- liri.js
+++ liri.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
 
 //connecting keys.js to liri.js
-var keys = require("./keys.js");
+var keys = require("./keys.js")
 
 //looking for npm
 var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var fs = require("fs");
 
 //getting spotify keys
 var spotify = new Spotify(keys.spotify);
+var argument = process.argv[2];
+var stringArgv = process.argv;
+var userConcert = "";
+var userSong = "";
+var userMovie = "";
 
-//
-if (selection === "concert-this"){
-  myConcert();
-}else if (selection === "spotify-this-song"){
-  mySpotify();
-}else if (selection === "movie-this"){
-  omdb();
-} else if (selection === "do-what-it-says") {
-  doWhatItSays();
+for (var i = 3; i < stringArgv.length; i++) {
+  if (i > 3 && i < stringArgv.length) {
+      userConcert = userConcert + " " + stringArgv[i];
+      userSong = userSong + " " + stringArgv[i];
+      userMovie = userMovie + "+" + stringArgv[i];
+  } else {
+      userConcert += stringArgv[i];
+      userSong += stringArgv[i];
+      userMovie += stringArgv[i];
+  }
 }
-
-//Bands in town
-
-var myConcert = function(){
-    
-}
-
