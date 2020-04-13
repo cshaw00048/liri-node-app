a8b2b30f478b09a9a586afbdac5a29e2b3470594
Christian Shaw <cshaw00048@gmail.com>
2020-04-13 08:08:28 -0500

    updating liri.js

--- liri.js
+++ liri.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
+
+//connecting keys.js to liri.js
 var keys = require("./keys.js");
+
+//looking for npm
 var axios = require("axios");
+
+//getting spotify keys
 var spotify = new Spotify(keys.spotify);
 
+//
+if (selection === "concert-this"){
+  myConcert();
+}else if (selection === "spotify-this-song"){
+  mySpotify();
+}else if (selection === "movie-this"){
+  omdb();
+} else if (selection === "do-what-it-says") {
+  doWhatItSays();
+}
 
+//Bands in town
 
-
-
+var myConcert = function(){
+    
+}
 
